0b2f374c9126289b82dc10d54efc11574f68c9ac
Ophus Wong <ophus.wong@gmail.com>
2018-01-22 17:14:18 -0700

    Add render for Search component Import Search component into App

---
+++ src/components/Search/Search.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Search.css';
+
+class Search extends Component {
+  render() {
+    return (
+      <form action="">
+        <input type="text" placeholder="Search by District Name"/>
+      </form>
+    )
+  }
+}
+
+export default Search;
