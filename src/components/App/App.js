21b828a39a7a81faa530ec6b6982c3808602137f
Ophus Wong <ophus.wong@gmail.com>
2018-01-22 18:00:30 -0700

    Add CompareContainer component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Search from '../Search/Search';
 import CardContainer from '../CardContainer/CardContainer';
+import CompareContainer from '../CompareContainer/CompareContainer';
 
 class App extends Component {
   constructor(props) {
@@ -16,6 +17,7 @@ class App extends Component {
       <div>
         <h1>Welcome To Headcount 2.0</h1>
         <Search />
+        <CompareContainer />
         <CardContainer districts={this.state.districts}/>
       </div>
     );
