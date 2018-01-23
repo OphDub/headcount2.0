deb12bfe6cdc4027412af68be595ee87279b95b8
Ophus Wong <ophus.wong@gmail.com>
2018-01-22 17:21:49 -0700

    Add return for CardContainer component Add return for Card component

---
+++ src/components/CardContainer/CardContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './CardContainer.css';
+import Card from '../Card/Card';
+
+const CardContainer = () => {
+  return (
+    <div>
+      Card Container
+    </div>
+  )
+}
+
+export default CardContainer;
