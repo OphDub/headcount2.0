d4937bd560e4c9c71e68debdc3cac4df4b0441bf
Jeff Bender <j25bender@gmail.com>
2018-01-24 12:32:42 -0700

    highFive check and css change

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,11 +3,15 @@ import './Card.css';
 
 const Card = ({ location, data }) => {
   // <article onClick={() => selectCard(location)}> ?
-  console.log(data)
+  const eachCard = Object.keys(data).map( dataPoint => {
+                                            const highOrLow = data[ dataPoint ] >= 0.5 ? "highFive" : "lowFive";
+                                            
+                                            return <li className={ highOrLow } >{ dataPoint + ': ' + data[ dataPoint ] }</li>
+                                        } )
   return (
     <article>
       <h4>{ location }</h4>
-      <ul>{ Object.keys(data).map( dataPoint => <li>{ dataPoint + ': ' + data[ dataPoint ] }</li> ) }</ul>
+      <ul>{ eachCard }</ul>
     </article>
   )
 }
