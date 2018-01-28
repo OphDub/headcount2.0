12400da3ebe6e40cebc9b3b7317d0c329e8eef76
Jeff Bender <j25bender@gmail.com>
2018-01-28 14:22:17 -0700

    fix compare card update

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -31,25 +31,21 @@ class App extends Component {
   }
 
   selectCard = (id) => {
-    // this.addCss(id)
     const foundDistrict = this.state.allDistricts.find((district)=> district.location === id)
     const newFoundDistrictArray = [...this.state.comparedDistricts, foundDistrict]
     const stateLocations = this.state.comparedDistricts.map( district => district.location)
 
-    // this.addCss(id)
     if (!stateLocations.includes(foundDistrict.location) && newFoundDistrictArray.length <= 2 ) {
       this.setState({ comparedDistricts: newFoundDistrictArray })
 
       this.compareCardData(newFoundDistrictArray);
     } else {
-      const oldArray = this.state.comparedDistricts
-
-      oldArray.shift()
-      const newArray = [...oldArray, foundDistrict]
-
-      this.setState({ comparedDistricts: newArray });
+      const newestCardData = newFoundDistrictArray[2]
+      const newestCardDataUpdate = [newestCardData]
+      
+      this.setState({ comparedDistricts: newestCardDataUpdate,
+                      comparisonObj: {} });
     }
-
   }
 
   compareCardData = (newFoundDistrictArray) => {
@@ -60,25 +56,18 @@ class App extends Component {
     }
   }
 
-  // addCss = (id) => {
-  //   if(document.getElementById(id).className === "unselected" && document.getElementById(id).parentElement.className !== 'compareContainer') {
-  //     document.getElementById(id).setAttribute("class", "selected")
-
-  //   } else if (document.getElementById(id).className === "selected" && document.getElementById(id).parentElement.className === 'cardContainer'){
-  //     document.getElementById(id).setAttribute("class", "unselected")
-  //   }
-  // }
-
   render() {
     console.log('comparisonObj',this.state.comparisonObj)
     return (
       <div>
         <h1>Welcome To Headcount 2.0</h1>
         <Search filterDistricts={this.filterDistricts} />
-        <CompareContainer comparedDistricts={this.state.comparedDistricts} 
-                          selectCard={this.selectCard} 
+        <CompareContainer comparedDistricts={this.state.comparedDistricts}
+                          selectCard={this.selectCard}
                           comparisonObj={this.state.comparisonObj} />
-        <CardContainer districts={this.state.allDistricts} selectCard={this.selectCard} addCss={this.addCss}/>
+        <CardContainer  districts={this.state.allDistricts}
+                        selectCard={this.selectCard}
+                        comparedDistricts={this.state.comparedDistricts}/>
       </div>
     );
   }
