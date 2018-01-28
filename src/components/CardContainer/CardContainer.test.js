dc2b010770d29e55b58123612cbe9f7a9a166769
Jeff Bender <j25bender@gmail.com>
2018-01-28 10:19:13 -0700

    card container tests

--- src/components/CardContainer/CardContainer.test.js
+++ src/components/CardContainer/CardContainer.test.js
@@ -1,13 +1,95 @@
 import React from 'react';
+// import ReactDOM from 'react-dom';
 import CardContainer from './CardContainer';
 import Card from '../Card/Card';
 import { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+// import DistrictRepository from '../../helper';
+// import kinderData from '../../data/kindergartners_in_full_day_program';
 
 describe('CARD CONTAINER', () => {
+  let wrapper;
+  // let masterDistrict;
+  let mockSelectFunc;
+  let mockAddCssFunc;
+  let districtArr;
 
-  test.skip('it should match the snapshot', () => {
-    const renderedComponent = shallow(<CardContainer />);
-    expect(renderedComponent).toMatchSnapshot();
+  beforeEach(() => {
+    // masterDistrict = new DistrictRepository(kinderData);
+    mockSelectFunc = jest.fn();
+    mockAddCssFunc = jest.fn();
+    districtArr = [ { location: 'CAMPO RE-6',
+    data:
+     { '2004': 0,
+       '2005': 0,
+       '2006': 1,
+       '2007': 1,
+       '2008': 1,
+       '2009': 1,
+       '2010': 1,
+       '2011': 1,
+       '2012': 1,
+       '2013': 1,
+       '2014': 1 } },
+  { location: 'CANON CITY RE-1',
+    data:
+     { '2004': 0.302,
+       '2005': 0.353,
+       '2006': 0.119,
+       '2007': 0.218,
+       '2008': 0.766,
+       '2009': 0.98,
+       '2010': 0.993,
+       '2011': 0.993,
+       '2012': 1,
+       '2013': 1,
+       '2014': 1 } },
+  { location: 'CENTENNIAL R-1',
+    data:
+     { '2004': 0,
+       '2005': 0.105,
+       '2006': 0.045,
+       '2007': 0,
+       '2008': 1,
+       '2009': 1,
+       '2010': 1,
+       '2011': 1,
+       '2012': 1,
+       '2013': 1,
+       '2014': 1 } },
+  { location: 'CENTER 26 JT',
+    data:
+     { '2004': 1,
+       '2005': 1,
+       '2006': 1,
+       '2007': 1,
+       '2008': 1,
+       '2009': 1,
+       '2010': 1,
+       '2011': 1,
+       '2012': 1,
+       '2013': 1,
+       '2014': 1 } } ];
+    wrapper = shallow(<CardContainer districts={districtArr} selectCard={mockSelectFunc} addCss={mockAddCssFunc}/>);
+  });
+
+  it('it should exist', () => {
+    expect(wrapper).toBeDefined()
   })
-})
+
+  it('should contain the correct number of Cards',() => {
+    const districtsArrLength = districtArr.length
+    expect(wrapper.find(Card).length).toEqual(districtsArrLength)
+  });
+  
+  it('should match snap of cardContainer', () => {
+    expect(wrapper).toMatchSnapshot();
+  })
+
+  it('should return correct last district card component',() => {
+    let { location, data, id } = districtArr[3]
+     expect(wrapper.find(Card).get(3).props.location).toEqual("CENTER 26 JT")
+     expect(wrapper.find(Card).get(3).props.data).toEqual({"2004": 1, "2005": 1, "2006": 1, "2007": 1, "2008": 1, "2009": 1, "2010": 1, "2011": 1, "2012": 1, "2013": 1, "2014": 1})
+     expect(wrapper.find(Card).get(3).props.id).toEqual("CENTER 26 JT")
+  })
+});
