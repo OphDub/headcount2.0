cd1ccb1d3348e96fc66389951b987020f8671ff1
Jeff Bender <j25bender@gmail.com>
2018-01-28 23:18:59 -0700

    comparision container tests

--- src/components/CompareContainer/CompareContainer.test.js
+++ src/components/CompareContainer/CompareContainer.test.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import CompareContainer from './CompareContainer';
 import Card from '../Card/Card';
+import App from '../App/App'
 import { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import DistrictRepository from '../../helper';
+import kinderData from '../../data/kindergartners_in_full_day_program';
 
 describe('COMPARE CONTAINER', () => {
   let wrapper;
   let mockSelectFunc;
   let comparedDistrictsArr;
   let comparisonObj;
+  let renderedComponent;
+  let masterDistrict;
 
   beforeEach(() => {
+    renderedComponent = shallow(<App />)
+    masterDistrict = new DistrictRepository (kinderData);
     mockSelectFunc = jest.fn();
     comparisonObj = {'CAMPO RE-6': 0.657, 'CANON CITY RE-1': 0.709, compared: 0.927}
     comparedDistrictsArr = [ { location: 'CAMPO RE-6',
@@ -46,31 +53,19 @@ describe('COMPARE CONTAINER', () => {
     expect(wrapper).toBeDefined()
   })
 
-  // test.skip('it should match the snapshot', () => {
-  //   const renderedComponent = shallow(<CompareContainer />);
-  //   expect(renderedComponent).toMatchSnapshot();
-  // })  
+  it('it should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  })
+
+  it('should contain the correct number of Cards',() => {
+    expect(wrapper.find(Card).length).toEqual(2)
+  })
+
+  it('should render compare cards correctly',() => {
+    const newComparisonObj = masterDistrict.compareDistrictAverages(comparedDistrictsArr[0].location, 
+    comparedDistrictsArr[1].location);
+    renderedComponent.setState({ newComparisonObj });
+    
+    expect(wrapper).toMatchSnapshot();
+  })
 })
-
-  
-
-//   it('it should exist', () => {
-//     expect(wrapper).toBeDefined()
-//   })
-
-//   it('should match snap of cardContainer', () => {
-//     expect(wrapper).toMatchSnapshot();
-//   })
-
-//   it('should contain the correct number of Cards',() => {
-//     const districtsArrLength = districtArr.length
-//     expect(wrapper.find(Card).length).toEqual(districtsArrLength)
-//   });
-
-//   it('should return correct last district card component',() => {
-//     let { location, data, id } = districtArr[3]
-//      expect(wrapper.find(Card).get(3).props.location).toEqual("CENTER 26 JT")
-//      expect(wrapper.find(Card).get(3).props.data).toEqual({"2004": 1, "2005": 1, "2006": 1, "2007": 1, "2008": 1, "2009": 1, "2010": 1, "2011": 1, "2012": 1, "2013": 1, "2014": 1})
-//      expect(wrapper.find(Card).get(3).props.id).toEqual("CENTER 26 JT")
-//   })
-// });
