434bea80d4fdd6b8227d56a4839f7e5cc083e520
Ophus Wong <ophus.wong@gmail.com>
2018-01-29 08:58:06 -0700

    Finish tests for App

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -77,7 +77,7 @@ describe('APP', () => {
     expect(renderedComponent.find('article').length).toEqual(181)
   })
 
-  it.skip('when cards are clicked they should be stored in state and rendered to the page', () => {
+  it('when cards are clicked they should be given the class selected and also placed in state', () => {
     const wrapper = mount(<App />)
     const lastMockDist = {
       data: {2004:0, 2005:1, 2006:1, 2007:1 ,2008:1,2009:1,2010:1,2011:1,2012:1,2013:1,2014:1},
@@ -92,10 +92,11 @@ describe('APP', () => {
     secondDistrict.simulate('click')
 
     expect(wrapper.state().comparedDistricts).toEqual(mockedCardArray)
-    // expect(wrapper.find('compareContainer').children().length).toEqual(3)
+    expect(wrapper.find('article.selected').last().hasClass('selected')).toEqual(true)
+    expect(wrapper.find('article.selected').length).toEqual(4)
   })
 
-  it.only('when two cards are compared it should create a comparison object in state', () => {
+  it('when two cards are compared it should create a comparison card and render it to the page', () => {
     const wrapper = mount(<App />)
     const lastMockDist = {
       data: {2004:0, 2005:1, 2006:1, 2007:1 ,2008:1,2009:1,2010:1,2011:1,2012:1,2013:1,2014:1},
@@ -114,9 +115,10 @@ describe('APP', () => {
     firstDistrict.simulate('click')
     secondDistrict.simulate('click')
 
-    expect(wrapper.state().comparisonObj).toEqual(mockCompObj)
     expect(wrapper.find('article.selected').last().hasClass('selected')).toEqual(true)
     expect(wrapper.find('article.selected').length).toEqual(4)
+    expect(wrapper.state().comparisonObj).toEqual(mockCompObj)
+    expect(wrapper.find('.comparisonCard').length).toEqual(1)
   })
 })
 
